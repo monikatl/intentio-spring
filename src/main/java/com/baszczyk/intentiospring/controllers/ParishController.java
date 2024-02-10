e91da9099192d35f1f553658c956c4475c56e874
monikatl <monika.joanna.baszczyk@gmail.com>
2024-02-10 11:14:44 +0100

    Fix parish path

--- src/main/java/com/baszczyk/intentiospring/controllers/ParishController.java
+++ src/main/java/com/baszczyk/intentiospring/controllers/ParishController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/parish")
+@RequestMapping()
 public class ParishController {
   @Autowired
   private ParishService parishService;
